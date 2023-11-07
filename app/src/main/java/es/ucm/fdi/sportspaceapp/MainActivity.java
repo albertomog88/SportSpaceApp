d770abb5aa9bda56727c2eba407277e53c8c2535
albertomog88 <almore03@ucm.es>
2023-11-07 20:06:51 +0100

    Inicio
    Creación de proyecto y vistas iniciales

--- app/src/main/java/es/ucm/fdi/sportspaceapp/MainActivity.java
+++ app/src/main/java/es/ucm/fdi/sportspaceapp/MainActivity.java
@@ -2,13 +2,24 @@ package es.ucm.fdi.sportspaceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void toRegistro(View v){
+        startActivity(new Intent(this, RegistroActivity.class));
+
+
     }
 }
